7c624473fa0da5f8cbbf52f00e98444ce29c5534
Dija <145768761+dija-64@users.noreply.github.com>
2024-01-20 17:04:15 +0000

    Added quiz questions

---
+++ starter/assets/js/questions.js
@@ -0,0 +1,31 @@
+const questions = [
+    {
+        question: "What is the name of the possessed girl in the film 'The Exorcest'?",
+        options: ["Jessica", "Regan", "Maya", "Yasmina"],
+        correctAnswer: "Regan"
+    },
+    {
+        question: "What is the name of the main villain in the 'Halloween' film franchise?",
+        options: ["Michael Myers", "Jason Vorhees", "Freddy Kreuger", "Leatherface"],
+        correctAnswer: "Michael Myers"
+    },
+
+    {
+        question: "What happens to anyone who watches a certain cursed videotape in the film 'The Ring'?",
+        options: ["They meet their Doppleganger", "They develop life-altering super powers", "They die in seven days", "They lose four out of five of their senses"],
+        correctAnswer: "They die in seven days"
+    },
+
+    {
+        question: "Which film features a haunted hotel with the room number 237 and a character named JAck Torrance?",
+        options: ["The Omen", "Paranormal Activity", "Sinister", "The Shining"],
+        correctAnswer: "Michael Myers"
+    },
+
+    {
+        question: "What supernatural entity is the main antaonist in the film 'The Conjuring'?",
+        options: ["Demons", "Cenobites", "Witches", "Zombies"],
+        correctAnswer: "Demons"
+    },
+];
+
